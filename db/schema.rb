8459fa96de8938e4850a10406f9608451ac139df
Gasper <bricktowntw@gmail.com>
2018-01-08 15:17:09 +0800

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108015455) do
+ActiveRecord::Schema.define(version: 20180108071256) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "restaurant_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -21,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180108015455) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
