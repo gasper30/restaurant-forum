33523aa395e128dd368c7d52e6df925f1ad04c76
Gasper <bricktowntw@gmail.com>
2018-01-08 09:32:30 +0800

    add restaurant show action and view for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -11,6 +11,10 @@ def new
     @restaurant = Restaurant.new
 end
 
+def show
+   @restaurant = Restaurant.find(params[:id])
+end
+
 def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
